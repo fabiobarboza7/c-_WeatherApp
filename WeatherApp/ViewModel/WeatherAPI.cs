1e942e3ab22a79f79a8b520a0cd29d0de91e565f
Fabio Souza <fabiobarboza7@hotmail.com>
2019-09-12 18:16:14 -0300

    fixing bugs, creating WeatherVM

--- WeatherApp/ViewModel/WeatherAPI.cs
+++ WeatherApp/ViewModel/WeatherAPI.cs
@@ -9,9 +9,9 @@ using WeatherApp.Model;
 
 namespace WeatherApp.ViewModel
 {
-    class WeatherAPI
+    public class WeatherAPI
     {
-        public const string API_KEY = "	z0H1hjJOGTRoGfvVUfe6VmlSQ5QVGy6l";
+        public const string API_KEY = "z0H1hjJOGTRoGfvVUfe6VmlSQ5QVGy6l";
         public const string BASE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/{0}?apikey={1}&metric=true";
 
         public static async Task<AccuWeather> GetWeatherInformationAsync(string locationKey)
